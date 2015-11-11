4913aeb4ed0a5f62d73a9601fbcdfc2ea272e221
Chris Bell <chris@mangohill.net>
2015-11-11 16:15:43 +0000

    New Challenge

---
+++ implement_interface/index.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Implement AnswerInterface and get Question to echo "4".
+
+(Credits to: https://github.com/wearebase/base-php-challenges/)
+*/
+class Question
+{
+public function __construct(AnswerInterface $answer)
+{
+echo "What is 2 + 2?\n";
+$answer = $answer->get()->the()->answer();
+if ($answer instanceof AnswerInterface) {
+echo $answer . PHP_EOL;
+}
+}
+}
+interface AnswerInterface
+{
+public function get();
+public function the();
+public function answer();
+}
+// start editing here
+
+
+// end editing here
+$answer = new Answer;
+$question = new Question($answer);
